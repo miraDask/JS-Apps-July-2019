3938beeb188e474abf61d3fa9bca276f07e92691
miraDask <mira.daskalov@gmail.com>
2019-07-23 17:05:58 +0300

    adding functionality

--- 04.ROUTING AND ARCHITECTURE/MySkeleton/scripts/app.js
+++ 04.ROUTING AND ARCHITECTURE/MySkeleton/scripts/app.js
@@ -1,1 +1,20 @@
+const app = Sammy("#root", function () {
 
+    this.use('Handlebars', 'hbs');
+
+    // Home
+    this.get('#/home', homeController.getHome);
+
+    // User
+    this.get('#/register', userController.getRegister);
+    this.post('#/register', userController.postRegister);
+
+    this.get('#/login', userController.getLogin);
+    this.post('#/login', userController.postLogin);
+   
+    this.get('#/logout', userController.logout);
+});
+
+(() => {
+    app.run('#/home');
+})();
