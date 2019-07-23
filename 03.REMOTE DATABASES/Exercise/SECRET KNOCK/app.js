ac4ff86570af2b38666551e13f58713904097313
miraDask <mira.daskalov@gmail.com>
2019-07-23 11:13:14 +0300

    refactoring - task completed

--- 03.REMOTE DATABASES/Exercise/SECRET KNOCK/app.js
+++ 03.REMOTE DATABASES/Exercise/SECRET KNOCK/app.js
@@ -1,49 +1,72 @@
 function knocking() {
     const username = 'guest';
     const password = 'guest';
-    let queryValue = 'Knock Knock';
-    const knockMessage = ' Knock again...'
+    let queryValue = 'Knock Knock.';
     const baseUrl = 'https://baas.kinvey.com/appdata/kid_BJXTsSi-e/knock';
     const loginUrl = 'https://baas.kinvey.com/user/kid_BJXTsSi-e/login';
-    
+
     const loginAuthString = 'a2lkX0JKWFRzU2ktZTo0NDdiOGU3MDQ2ZjA0ODAzOWQ5NTYxMGMxYjAzOTM5MA==';
     const btnPush = document.querySelector('button');
-    const h2 = document.getElementById('result');
+    const result = document.getElementById('result');
+    let counter = 1;
+    let authToken = '';
 
-    login();
+    run();
 
-    async function login() {
+    async function run() {
         try {
-            const response = await fetch(loginUrl, {
-                method: 'POST',
-                headers: {
-                    'Authorization': `Basic ${loginAuthString}`,
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    "username": username,
-                    "password": password
-                })
-            })
+            const loginData = await login();
+            authToken = loginData._kmd.authtoken;
 
-            const data = await responseHandler(response);
-            btnPush.hidden = false;
-            const authToken = data._kmd.authtoken;
-            btnPush.addEventListener('click', async () => {
-                h2.textContent = '';
-                const query = `?query=${queryValue}`;
-                const data = await sendGetRequest(authToken, query);
-                if (!data) {
-                    btnPush.textContent = 'Welcome!'
-                    return;
-                }
+            if (counter === 1) {
+                btnPush.hidden = false;
+            }
 
-                queryValue = data.answer;
-                h2.textContent = queryValue + knockMessage;
-
-            });
+            btnPush.addEventListener('click', handleKnocking);
         } catch (err) {
             alert(err.message)
+        }
+    }
+
+    async function login() {
+        const response = await fetch(loginUrl, {
+            method: 'POST',
+            headers: {
+                'Authorization': `Basic ${loginAuthString}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "username": username,
+                "password": password
+            })
+        })
+
+        return responseHandler(response);
+    }
+
+    async function handleKnocking() {
+        while (true) {
+            if (counter > 1) {
+                btnPush.hidden = true;
+            }
+            const query = `?query=${queryValue}`;
+            const response = await sendGetRequest(authToken, query);
+
+            queryValue = response.message;
+
+            result.innerHTML += (`
+        <h1>${response.answer}</h1>
+    `)
+            if (response.message) {
+                result.innerHTML += (`
+        <h1>${queryValue}</h1>
+    `)
+            }
+
+            if (!response.message) {
+                break;
+            }
+            counter++;
         }
     }
 
